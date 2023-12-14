0098c481abfcc7d255ab7d40ea2343e77827ade2
Aparna Mishra <aparna.m699@gmail.com>
2023-12-14 13:18:37 +0530

    order card history

---
+++ src/Component/Order/OrderHistory/OrderCard.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { orderAction } from '../../../store/Order/orderSlice';
+
+export const OrderCard = (props) => {
+    const [orders , setOrders] = useState([]);
+
+    const orderId = props.order.id;
+    const totalCost = props.order.totalCost;
+    const orderDate = new Date(props.order.orderDate);
+    const status = props.order.statusId
+    
+    const orderList = useSelector((state) => state.order)
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(orderAction.getOrderItems(orderId));
+    }, []);
+    useEffect(() => {
+      if(!orderList.isOrderListLoading && orderList.isOrderListSuccess){
+        setOrders(orderList.orderLines.filter((k) => k.orderId.id == orderId))
+      }
+    }, [orderList]);
+
+  return (
+    <div className='card p-2 bg-light my-2'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <h5><spam className="fw-bold">Order ID: </spam>{orderId}</h5>
+          </div>
+          <div className='col-md-6'>
+            <h6><spam className="fw-bold">Total Cost: </spam>{'Rs. ' + totalCost.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</h6>
+          </div>
+          <div className='col-md-3'>
+            <h6 className='text-end'><spam className="fw-bold">Ordered On: </spam>{orderDate.getDate()+" "+orderDate.toLocaleString('en-us',{month:'short'})+" "+orderDate.getFullYear()}</h6>
+          </div>
+          <div className='col-md-3'>
+            <h6 className='text-end'><spam className="fw-bold">Order Status: </spam>{status}</h6>
+          </div>
+        </div>
+        <div className="card"></div>
+        <div className="row row-flex">
+        <div className='col-md-10'>
+          {console.log("i", orderId)}
+          <div className="row row-flex p-3">
+            {orders.map((k) => {
+              return( 
+                <div className=' col-md-2'>
+                  <div className="card card-img">
+                    <a href={`/items/${k.itemId.id}`}> 
+                    <img
+                      src={k.itemId.imgUrl[0]}
+                      class="card-img-top"
+                      alt="..."
+                      style={{height: "150px"}}
+                    />
+                    </a>
+                    </div>
+                    <div className="card-body-text p-1">
+                      <p className='fs-6 text'>{k.itemId.productName}</p>
+                    </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className='d-flex justify-content-end p-2'>
+            <button className='btn btn-outline-dark' >Track order</button>
+          </div>
+        </div>
+        </div>
+        
+    </div>
+  )
+}
